991d9bebcce3b9f15490ed2493f63fb78ccbfc5f
Mark Yuan <weerspecial@126.com>
2016-03-30 22:24:46 +0800

    Watch mechanism update

--- assets/data/watch.js
+++ assets/data/watch.js
@@ -14,10 +14,10 @@
 			model: d,
 			changes: {},
 			addchange: function (c) {
-				var chlist = h.changes[c.name];
+				var chlist = this.changes[c.name];
 				if (!chlist) {
 					chlist = [c.change];
-					h.changes[c.name] = chlist;
+					this.changes[c.name] = chlist;
 				} else {
 					for (var i = 0; i < chlist.length; i++) {
 						var fn = chlist[i];
@@ -33,10 +33,23 @@
 		watchlist[watchlist.length] = h;
 		return h;
 	}
+	function getforward(c) {
+		var el = null;
+		if (c.forward) {
+			var t = typeof (c.forward);
+			if (t == 'string') {
+				el = document.getElementById(c.forward);
+			} else if (t == 'function') {
+				el = c.forward();
+			} else {
+				el = c.forward;
+			}
+		}
+		return el;
+	}
 	var dhandler = {
 		typechange: function (p, c) {
-			function vchange(event) {
-				//console.log(event.keyCode);
+			function evtchange(event) {
 				if (!event.keyCode) {
 					p[c.name] = this.value;
 				} else if (!event.altKey && !event.ctrlKey) {
@@ -49,62 +62,118 @@
 					}
 				}
 			}
+			var el = getforward(c);
+			if (el) {
+				el.addEventListener('onkeyup', evtchange);
+				el.addEventListener('onchange', evtchange);
+				el.addEventListener('onblur', evtchange);
+			} else {
+				console.log('Forward target missing');
+			}
+		}, attrchange: function (p, c) {
+			// select the target node
+			var target = getforward(c);
+			if (target) {
+				var args = c.args ? c.args.init : null;
+				if (args) {
+					target.addEventListener(args.evt, function (event) {
+						this.setAttribute(args.prop, this[args.prop]);
+						//console.log(this.value);
+					});
+				}
+				// create an observer instance
+				var observer = new MutationObserver(function (mutations) {
+					mutations.forEach(function (mutation) {
+						if (mutation.type == 'attributes') {
+							var target = mutation.target;
+							p[c.name] = target.getAttribute(mutation.attributeName);
+						}
+					});
+				});
 
-			var el = document.getElementById('text');
-			el.onkeyup = vchange;
-			el.onchange = vchange;
-			el.onblur = vchange;
+				// configuration of the observer:
+				var config = { attributes: true, childList: true, characterData: true };
+
+				// pass in the target node, as well as the observer options
+				observer.observe(target, config);
+
+				// later, you can stop observing
+				//observer.disconnect();
+			} else {
+				console.log('Forward target missing');
+			}
+		}, propchange: function (p, c) {
+			// select the target node
+			var target = getforward(c);
+			if (target) {
+				watchp(target, {
+					name: 'value'
+				}, [function (model, property, value, origin) {
+					console.log(model);
+					console.log(property);
+					console.log(value);
+				}]);
+			} else {
+				console.log('Forward target missing');
+			}
 		}
 	}
-	function watch(d, c, undef) {
-		function gethandler(key) {
-			if (!key) {
-				return null;
-			}
-			var t = typeof (key);
-			console.log(t);
-			if (t == 'string') {
-				return dhandler[key];
-			} else {
-				return key;
-			}
+	function gethandler(key) {
+		if (!key) {
+			return null;
 		}
+		var t = typeof (key);
+		if (t == 'string') {
+			return dhandler[key];
+		} else {
+			return key;
+		}
+	}
+	function watchp(d, c, chlist) {
+		var ov = d[c.name], nv = ov;
+		if (delete d[c.name]) {
+			if (Object.defineProperty) {
+				Object.defineProperty(d, c.name, {
+					get: function () {
+						return nv;
+					},
+					set: function (val) {
+						if (nv == val) {
+							console.log('No change:' + val);
+						} else {
+							if (chlist && chlist.length > 0) {
+								for (var i = 0; i < chlist.length; i++) {
+									var fn = chlist[i];
+									fn(d, c.name, val, nv);
+								}
+							}
+							nv = val;
+						}
+					}
+				});
+				var initer = gethandler(c.init);
+				if (initer) {
+					initer(d, c);
+				}
+				d[c.name] = nv;
+				return true;
+			} else {
+				console.log('Define property not supported');
+				debugger;
+			}
+		} else {
+			console.log('Cannot watch ' + c.name);
+			debugger;
+		}
+		return false;
+	}
+	function watch(d, c, undef) {
 		var ov = d[c.name], nv = ov;
 		var h = findhandler(d);
 		var initer = gethandler(c.init);
 		if (!h) {
 			h = createhandler(d, c);
-			if (delete d[c.name]) {
-				if (Object.defineProperty) {
-					Object.defineProperty(d, c.name, {
-						get: function () {
-							return nv;
-						},
-						set: function (val) {
-							var chlist = h.changes[c.name];
-							if (chlist && chlist.length > 0) {
-								for (var i = 0; i < chlist.length; i++) {
-									var fn = chlist[i];
-									fn(d, c.name, val, ov);
-								}
-							}
-							nv = val;
-							ov = val;
-						}
-					});
-					if (initer) {
-						initer(d, c);
-					}
-					d[c.name] = nv;
-					return true;
-				} else {
-					console.log('Define property not supported');
-					debugger;
-				}
-			} else {
-				console.log('Cannot watch ' + c.name);
-				debugger;
-			}
+			watchp(d, c, h.changes[c.name]);
 		} else {
 			h.addchange(c);
 			if (initer) {
