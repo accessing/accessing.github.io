01b9cf54688aa9f3b69afdebd76a5caf7c84531e
mind0n <weerspecial@gmail.com>
2016-06-11 10:13:32 +0800

    Pre-migration

--- assets/bundle/mindexpress.js
+++ assets/bundle/mindexpress.js
@@ -12844,6 +12844,367 @@ function overlay(c) {
 		return r;
 	}();
 })();
+joy.creators.ui = {
+	create: function (n, j, c) {
+		var el = null;
+		if (this[n]) {
+			el = this[n](j, c);
+		}
+		return el;
+	}
+  , created: function (el) {
+  }
+  , append: function (p, c) {
+  	if (p && c && p.tagName && (c.tagName || c.nodeName)) {
+  		if (p.append) {
+  			p.append(c);
+  		} else {
+  			p.appendChild(c);
+  		}
+  	}
+  }
+  , val: function (p, v) {
+  	if (p.setval) {
+  		p.setval(v);
+  	} else {
+  		var tn = document.createTextNode(v);
+  		p.appendChild(tn);
+  	}
+  }
+};
+joy.creators.ui.simpleMenu = function (ext) {
+	var json = {
+		tag: 'ul',
+		alias: 'menu',
+		className: 'simple-menu',
+		setval: function (data) {
+			if (data instanceof Array) {
+				for (var i = 0; i < data.length; i++) {
+					var iel = joy.jbuilder({ ui: 'simpleMenuItem' });
+					iel.setval(data[i]);
+					iel.$root = this.$root;
+					this.appendChild(iel);
+				}
+			}
+		},
+		getval: function () {
+			var rlt = [];
+			for (var i = 0; i < this.childNodes.length; i++) {
+				var item = this.childNodes[i]
+				rlt[rlt.length] = item.getval();
+			}
+			return rlt;
+		},
+		show: function (target) {
+			if (!target) {
+				return;
+			}
+			var rc = target.getBoundingClientRect();
+			var p = [rc.left, rc.bottom, rc.width];
+			this.style.position = 'fixed';
+			this.style.left = p[0] + 'px';
+			this.style.top = p[1] + 'px';
+			this.style.width = p[2] + 'px';
+			$(this).show();
+		},
+		hide: function () {
+			$(this).hide();
+		}
+	};
+	delete ext.ui;
+	joy.extend(json, ext);
+	var el = joy.jbuilder(json);
+	return el;
+};
+joy.creators.ui.simpleMenuItem = function (ext) {
+	var item = {
+		tag: 'li',
+		className: 'simple-menu-item',
+		getval: function () {
+			return this.$link.innerHTML;
+		},
+		setval: function (val) {
+			$(this.$link).text(val);
+		},
+		onclick: function () {
+			this.$root.select(this);
+		},
+		$: { tag: 'a', alias: 'link' }
+	};
+	delete ext.ui;
+	joy.extend(item, ext);
+	var el = joy.jbuilder(item);
+	return el;
+};
+
+joy.creators.ui.selector = function (ext) {
+	function toggle(e) {
+		var root = this.$root;
+		if ($(root.$menu).is(':visible')) {
+			console.log('visible');
+			this.$root.hideMenu();
+		} else {
+			console.log('hidden');
+			this.$root.showMenu();
+		}
+	}
+	var json = {
+		tag: 'div',
+		className: 'dropdown',
+		select: function (item) {
+			this.$txt.nodeValue = item.getval();
+			this.hideMenu();
+		},
+		selectedIndex: function () {
+			var txt = this.$txt.nodeValue;
+			var items = $(this.$menu).find('li');
+			for (var i = 0; i < items.length; i++) {
+				var item = items[i];
+				if (item.getval() == txt) {
+					return i;
+				}
+			}
+			return -1;
+		},
+		getval: function () {
+			return this.$txt.nodeValue;
+		},
+		setval: function (data) {
+			if (!data || (data instanceof Array && data.length < 1)) {
+				return;
+			}
+			var menu = this.$menu;
+			if (menu) {
+				destroy(menu);
+			}
+			menu = joy.jbuilder({ ui: 'simpleMenu' });
+			menu.$root = this;
+			this.$menu = menu;
+			//this.appendChild(menu);
+			document.body.appendChild(menu);
+			menu.setval(data);
+		},
+		showMenu: function () {
+			if (this.$menu) {
+				this.$menu.show(this);
+			}
+		},
+		hideMenu: function () {
+			if (this.$menu) {
+				this.$menu.hide();
+			}
+		},
+		$: [
+				{
+					tag: 'div', alias: 'btn', className: 'toggle'
+					, onclick: toggle
+					, $: [
+					  { tag: '#text', alias: 'txt', $: 'Select an option' },
+					  { tag: 'span', className: 'caret right' }
+					]
+				}
+		]
+	};
+	if (!ext) {
+		ext = {};
+	}
+	if (!ext.type || ext.type == 'dropdownlist') {
+
+	} else if (ext.type == 'dropdowneditor') {
+		delete json.$[0].onclick;
+		json.select = function (item) {
+			this.$txt.value = item.getval();
+			this.hideMenu();
+		};
+		json.$[0].$ = [
+			{ tag: 'input', type: 'text', value: 'Select an option', alias: 'txt' },
+			{
+				tag: 'div', alias: 'caret', className: 'caret-container'
+				, onclick: toggle
+				, $: { tag: 'span', className: 'caret' }
+			}
+		];
+	}
+	delete ext.ui;
+	joy.extend(json, ext);
+	var el = joy.jbuilder(json);
+	return el;
+}
+joy.cover = function (c) {
+	var hide = c.hide;
+	var target = c.target;
+	var fixed = false;
+	if (!target) {
+		target = document.body;
+		fixed = true;
+	}
+	var ovl = target.$ovl$;
+	var spos = false;
+	if (!ovl) {
+		ovl = document.createElement('div');
+		ovl.className = 'cover ';// + (fixed ? 'fixed' : '');
+		target.$ovl$ = ovl;
+
+		var initpos = target.astyle(['position']);
+		console.log('Cover: ' + initpos);
+		target.$position$ = initpos;
+
+		if (!fixed && initpos == 'static') {
+			target.style.position = 'relative';
+			spos = true;
+		}
+		target.appendChild(ovl);
+	}
+	if (hide) {
+		ovl.$displayed$ = false;
+		ovl.style.display = 'none';
+		if (ovl.$popup$) {
+			destroy(ovl.$popup$);
+		}
+		if (spos) {
+			target.style.position = target.$position$;
+		}
+	} else if (!ovl.$displayed$) {
+		ovl.$displayed$ = true;
+		ovl.style.display = '';
+		if (c.popup) {
+			var rect = ovl.getBoundingClientRect();
+			var el = joy.jbuilder(c.popup);
+			el.onclose = function () {
+				joy.cover({ hide: true, target: target });
+			}
+			if (ovl.$popup$) {
+				destroy(ovl.$popup$);
+			}
+			ovl.$popup$ = el;
+			var arg = { target: target, pos: [rect.left, rect.top, rect.width / 2, rect.height / 2, rect.right, rect.bottom], details: c.details };
+			if (fixed) {
+				el.style.position = 'fixed';
+			}
+			if (el.popup) {
+				el.popup(arg);
+			} else {
+				target.appendChild(el);
+				var rc = el.getBoundingClientRect();
+				el.style.left = arg.pos[2] - rc.width / 2 + 'px';
+				el.style.top = arg.pos[3] - rc.height / 2 + 'px';
+			}
+			touchable(el, { mode: 'zsizing' });
+			return el;
+		}
+	}
+};
+joy.creators.ui.staticPopup = function (ext) {
+	var json = {
+		tag: 'div',
+		className: 'popup',
+		setval: function (val) {
+			$(this).text(val);
+		},
+		built: function (json) {
+			if (json.type == 'imgloading') {
+				this.style.backgroundImage = 'url(img/icon/loading.gif)';
+				this.style.backgroundPosition = 'center';
+				this.style['background-size'] = '96px';
+				this.style.backgroundRepeat = 'no-repeat';
+				this.style.width = '64px';
+				this.style.height = '64px';
+			}
+		}
+	};
+	delete ext.ui;
+	joy.extend(json, ext);
+	var el = joy.jbuilder(json);
+	return el;
+};
+
+joy.creators.ui.formPopup = function (ext) {
+	var json = {
+		tag: 'div',
+		className: 'popup form',
+		setval: function (data) {
+			$(this.$caption).text(data.title);
+			if (data.content) {
+				if (this.$content$) {
+					destroy(this.$content$);
+				}
+				var el = data.content;
+				if (!el.tagName) {
+					el = joy.jbuilder(data.content);
+				}
+				this.$content$ = el;
+				this.$body.appendChild(el);
+				return el;
+			}
+			return null;
+		},
+		close: function () {
+			this.style.display = 'none';
+			if (this.onclose) {
+				this.onclose();
+			} else {
+				this.dispose();
+			}
+		},
+		dispose: function () {
+			if (this.$content$) {
+				destroy(this.$content$);
+			}
+		},
+		popup: function (arg) {
+			var parent = arg.target;
+			parent.appendChild(this);
+			var rc = this.getBoundingClientRect();
+			el.style.left = arg.pos[2] - rc.width / 2 + 'px';
+			el.style.top = arg.pos[3] - rc.height / 2 + 'px';
+			el.style.width = el.astyle(['width']);
+			el.style.height = el.astyle(['height']);
+		},
+		$: [
+			{
+				tag: 'div', alias: 'caption', className: 'caption', $target$: function () { return this.$root; }
+			}, {
+				tag: 'div', alias: 'bclose', className: 'btns', $: [
+					{
+						tag: 'div', className: 'btn bmax', onclick: function () {
+							if (!this.$state$) {
+								this.$state$ = {
+									left: this.$root.style.left,
+									top: this.$root.style.top,
+									width: this.$root.style.width,
+									height: this.$root.style.height
+								};
+								this.$root.style.width = '';
+								this.$root.style.height = '';
+								this.$root.style.left = '0px';
+								this.$root.style.top = '0px';
+								this.$root.style.right = '0px';
+								this.$root.style.bottom = '0px';
+							} else {
+								this.$root.style.width = this.$state$.width;
+								this.$root.style.height = this.$state$.height;
+								this.$root.style.left = this.$state$.left;
+								this.$root.style.top = this.$state$.top;
+								this.$root.style.right = '';
+								this.$root.style.bottom = '';
+								this.$state$ = null;
+							}
+						}, $: 'o'
+					},
+					{
+						tag: 'div', className: 'btn bclose', onclick: function () {
+							this.$root.close();
+						}, $: '×'
+					}
+				]
+			}, { tag: 'div', alias: 'body', className: 'body' }
+		]
+	};
+	delete ext.ui;
+	joy.extend(json, ext);
+	var el = joy.jbuilder(json);
+	return el;
+};
 function acts(a, t) {
 	if (!a) {
 		debugger;
